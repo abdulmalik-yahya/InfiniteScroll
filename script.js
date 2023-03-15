ae2a2d2a31201f0efad5a56220adfbaebdfad194
abdulmalik-yahya <dolphin.tc.ye@gmail.com>
2023-03-15 22:47:19 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const images = document.querySelectorAll(".image-container img");
+const viewImage = document.querySelector(".view-image");
+const displayedImage = document.querySelector(".view-image img");
+const closeBtn = document.querySelector(".view-image span");
+const imageContainer = document.querySelector(".image-container");
+
+images.forEach((image) => {
+    image.addEventListener("click", e => {
+        // console.log(e);
+        viewImage.style.display = "flex";
+        displayedImage.src = e.target.src;
+    })
+})
+
+closeBtn.addEventListener("click", () => viewImage.style.display = "none");
+
+const getImage = async (n) => {
+    for (let i = 0; i < n; i++) {
+        try {
+            const res = await fetch("https://source.unsplash.com/random");
+            // console.log(res);
+            let url = res.url;
+            // console.log(url);
+            const img = document.createElement('img'); // create img element
+            img.src = url; // set its src to the one we feteched
+            imageContainer.appendChild(img); // append it to the container
+
+            img.addEventListener("click", e => {
+                // console.log(e);
+                viewImage.style.display = "flex";
+                displayedImage.src = e.target.src;
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+}
+
+// now implement the infinite scroll using
+// clientheight, scrollHeight, scrolltop
+
+window.addEventListener('scroll', (e) => {
+    // console.log(e);
+    // destructure the required properties
+    const { clientHeight, scrollHeight, scrollTop } = e.target.documentElement;
+    // console.log(clientHeight, scro llTop, scrollHeight);
+    // now once the scroll is at the bottom of page, the clientHeight+scrolltop = scrollHeight
+    if (clientHeight + scrollTop == scrollHeight) {
+        // console.log("Reached Bottom");
+        getImage(3); // load new images once we reach bottom of page
+    }
+})
+
+getImage(9);
